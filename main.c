163e4993999020672ad6a6f6dab214d84c7e0a12
Krzysztof Kołodenny <dzidolos@gmail.com>
2017-10-13 12:03:09 +0200

    Add files via upload
    Program to read an amount (integer value) and break the amount into the smallest possible number of bank notes.
    Note: The possible banknotes are 100, 50, 20, 10.

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+Write a program to read an amount (integer value) and break the amount into smallest possible number of bank notes.
+Note: The possible banknotes are 100, 50, 20, 10.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int amt,total;
+    printf("Take a your amount: ");
+    scanf("%d",&amt);
+    total=amt/100;
+    printf("There are: ");
+	printf("\n%d Note(s) of 100.00\n", total);
+	amt=(amt-(total*100));
+	total=amt/50;
+    printf("\n%d Note(s) of 50.00\n",total);
+    amt=(amt-(total*50));
+	total=amt/20;
+    printf("\n%d Note(s) of 20.00\n",total);
+    amt=amt-(total*20);
+    total=amt/10;
+    printf("\n%d Note(s) of 10.00\n",total);
+    return 0;
+}
